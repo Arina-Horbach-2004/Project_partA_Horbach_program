0996abeffc4bf3d64d2530501d3f89f6d0e4f4ae
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:12:20 +0100

    Створення  інтерфейсу IPerson

---
+++ Project_partA_Horbach_program/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Horbach_program
+{
+    public interface IPerson
+    {
+        int Id { get; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        public string Tostring();
+    }
+}
