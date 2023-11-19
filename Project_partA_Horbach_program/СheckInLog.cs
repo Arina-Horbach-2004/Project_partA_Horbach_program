22551ab2bdab8cd344aa5ad578e35eb3b32a8bde
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:07:02 +0100

    Створення класу CheckInLog

---
+++ Project_partA_Horbach_program/СheckInLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Horbach_program
+{
+    public class CheckInLog
+    {
+        // Властивість для зберігання часу заселення
+        public DateTime CheckInTime { get; }
+
+        // Властивість для зберігання інформації про гостя
+        public Guest Guest { get; }
+
+        // Властивість для зберігання інформації про номер
+        public Room Room { get; }
+
+        // Властивість для зберігання інформації про персонал готелю, який провів заселення
+        public HotelStaff CheckedInBy { get; }
+
+        public CheckInLog(DateTime checkInTime, Guest guest, Room room, HotelStaff checkedInBy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
