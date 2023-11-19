266c2737f85e98342a9ac7de73483664f1a6af27
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:21:15 +0100

    Створення unit-тестів для класу CheckInLog

---
+++ TestProject1/CheckInLog.cs
@@ -0,0 +1,32 @@
+﻿using Project_partA_Horbach_program;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class CheckInLogTest
+    {
+        [TestMethod]
+        public void CheckInLogInitialization_ShouldSetPropertiesCorrectly()
+        {
+            // Arrange
+            var checkInTime = DateTime.Now;
+            var guest = new Guest("Ferit Korhan", "48538353801", new DateTime(1990, 1, 1));
+            var room = new Room(101, 150.0, RoomType.Single);
+            var staff = new HotelStaff("Admin", "48538353802", new DateTime(1990, 1, 1), StaffPosition.Administrator);
+
+            // Act
+            var checkInLog = new CheckInLog(checkInTime, guest, room, staff);
+
+            // Assert
+            Assert.AreEqual(checkInTime, checkInLog.CheckInTime);
+            Assert.AreEqual(guest, checkInLog.Guest);
+            Assert.AreEqual(room, checkInLog.Room);
+            Assert.AreEqual(staff, checkInLog.CheckedInBy);
+        }
+    }
+}
