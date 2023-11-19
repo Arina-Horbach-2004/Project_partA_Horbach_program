bcd1b3663edc2ce89789fa0241f3fa83316b6c7c
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:08:57 +0100

    Створення перелічення StaffPosition

---
+++ Project_partA_Horbach_program/StaffPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Horbach_program
+{
+    public enum StaffPosition
+    {
+        Administrator,
+        Staff,
+        Manager,
+        Receptionist,
+        Concierge,
+        RegistrationManager,
+        FrontDeskClerk,
+        Other
+    }
+}
