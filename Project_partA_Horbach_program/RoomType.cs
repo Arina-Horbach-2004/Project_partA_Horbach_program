43640e55867b85b1b5569f3a57ee532d2fe6a65c
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:10:20 +0100

    Створення перелічення RoomType

---
+++ Project_partA_Horbach_program/RoomType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Horbach_program
+{
+    public enum RoomType
+    {
+        Standard,
+        Suite,
+        Deluxe,
+        Luxury,
+        Familyroom,
+        Adjoiningroom,
+        Oceanview,
+        HoneymoonSuite,
+        BusinessClass,
+        Villa,
+        Garden_view,
+        Longstay,
+        Single,
+        Double,
+        Withseparatebeds
+    }
+}
