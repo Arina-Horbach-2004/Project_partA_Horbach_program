39cdf06db0c1cd12174da24d7c6a484ad381e475
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:03:42 +0100

    Створення класу Room

---
+++ Project_partA_Horbach_program/Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Horbach_program
+{
+    public class Room
+    {
+        // Властивість для номеру номера
+        public int RoomNumber { get; set; }
+
+        // Властивість для типу номера 
+        public RoomType Type { get; set; }
+
+        // Властивість, що вказує, чи зайнятий номер
+        public bool IsOccupied { get; set; }
+
+        // Властивість для розцінки за номер
+        public double Rate { get; set; }
+
+        public Room(int roomNumber, double rate, RoomType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Метод для заселення гостя в номер
+        public void CheckIn(Guest guest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
