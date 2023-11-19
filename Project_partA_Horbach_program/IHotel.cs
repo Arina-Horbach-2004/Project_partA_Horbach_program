5b77b99cfa4b107047b6dd6940e99ccb5127d763
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-19 18:13:21 +0100

    Cтворення  інтерфейсу IHotel

---
+++ Project_partA_Horbach_program/IHotel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Horbach_program
+{
+    public interface IHotel
+    {
+        // Реєстрація посетителя
+        public void RegisterGuest(Guest guest);
+
+        // Бронювання номеру
+        public void ReserveRoom(Guest guest, Room room);
+
+        // Видалення персоналу
+        public void RemoveStaff(HotelStaff staff);
+
+        void CheckIn(Guest guest, Room room, HotelStaff checkedInBy);
+
+        // Виселення посетителя
+        public void CheckOut(Guest guest);
+
+        // Отримати список сотрудників
+        List<HotelStaff> GetStaffList();
+
+        // Отримати список посетителів
+        List<Guest> GetGuestList();
+
+        // Отримати список номерів
+        List<Room> GetRoomList();
+
+        public string Tostring();
+    }
+}
